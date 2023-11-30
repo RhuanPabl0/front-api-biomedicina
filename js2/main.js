7875f1632a67dc1ee8f4664f6f3b270757635add
RhuanPabl0 <rhuanpnascimento@gmail.com>
2023-11-30 10:06:26 -0400

    Second Commit

---
+++ js2/main.js
@@ -0,0 +1,14 @@
+function realizarConsulta() {
+    alert('Consulta realizada com sucesso!');
+  }
+
+  function consultarHorarios() {
+    
+    alert('Consulta de horários disponíveis realizada!');
+  }
+
+
+
+  $(document).ready(function() {
+    $('#cpf').inputmask('999.999.999-99', { placeholder: '_._._._._._._._._' });
+});
